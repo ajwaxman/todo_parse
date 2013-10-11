9a8486a1653d6f75192249965fde52370069f2bb
Adam Waxman <awaxman11@gmail.com>
2013-10-11 16:46:50 -0400

    Add todo, login, and logout views

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -76,6 +76,297 @@ $(function() {
   // Todo Item View
   // --------------
 
-  
+  // The DOM eLement for a todo item...
+  var TodoView = Parse.View.extend({
+
+    // ... is a list tag
+    tagName: "li",
+
+    // Cache the template function for a signle item
+    template: _.template($('#item-template').html()),
+
+    // The DOM events specific to an item.
+    event: {
+      "click .toggle": "toggleDone",
+      "dblick label.todo-content": "edit",
+      "click .todo-destroy": "clear",
+      "keypress .edit": "updateOnEnter",
+      "blur .edit": "close"
+    },
+
+    // The TodoView listens for changes to its model, re-rendering. Since there's
+    // a one-to-one correspondence between a Todo and a TodoView in this
+    // app, we set a direct reference on the model for convenience.
+    initialize: function() {
+      _.bindAll(this, 'render', 'close', 'remove');
+      this.model.bind('change', this.render);
+      this.model.bind('destroy', this.remove);
+    },
+
+    // Re-render the contents of the todo item.
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      this.input = this.$('.edit');
+      return this;
+    },
+
+    // Toggle the '"done"' state of the model.
+    toggleDone: function() {
+      this.model.toggle();
+    },
+
+    // Switch this view into '"editing"' mode, displaying the input field.
+    edit: function() {
+      $(this.el).addClass("editing");
+      this.input.focus();
+    },
+
+    // Close the '"editing"' mode, saving changes to the todo.
+    close: function() {
+      this.model.save({content: this.input.val()});
+      $(this.el).removeClass("editing");
+    },
+
+    // If you hit 'enter, we're through editing the item.
+    updateOnEnter: function(e) {
+      if (e.keyCode == 13) this.close();
+    },
+
+    // Remove the item, destroy the model.
+    clear: function() {
+      this.model.destroy();
+    }
+
+  });
+
+  // The Application
+  // ---------------
+
+  // The main view that lets a user manage their todo items
+  var ManageTodosView = Parse.View.extend({
+
+    // Our template for the line of statistics at the bottom of the app.
+    statsTemplate: _.template($('#stats-template').html());
+
+    // Delegated events for creating new items, and clearing completed ones.
+    events: {
+      "keypress #new-todo": "createOnEnter",
+      "click #clear-completed": "clearCompleted", 
+      "click #toggle-all": "toggleAllComplete",
+      "click .log-out": "logOut",
+      "click ul#filters a": "selectFilter"
+    },
+
+    el: ".content",
+
+    // At initialization we bind to the relevant events on the 'Todos'
+    // collection, when items are added or changed. Kick things off by 
+    // loading any preexisting todos that might be saved to Parse.
+    initialize: function() {
+      var self = this;
+
+      _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'toggleAllComplete', 'logOut', 'createOnEnter');
+
+      // Main todo management template
+      this.$el.html(_.template($('#manage-todos-template').html()));
+
+      this.input = this.$('#new-todo');
+      this.allCheckbox = this.$('#toggle-all')[0];
+
+      // Create out collection of Todos
+      this.todos = new TodoList;
+
+      // Setup the query for the collection to look for todos from the current user
+      this.todos.query = new Parse.Query(Todo);
+      this.todos.query.equalTo("user", Parse.User.current());
+
+      this.todos.bind('add', this.addOne);
+      this.todos.bind('reset', this.addAll);
+      this.todos.bind('all', this.render);
+
+      // Fetch all the todo items for this user
+      this.todos.fetch();
+
+      state.on('change', this.filter, this);
+    },
+
+    // Logs out the user and shows the Login view
+    logOut: function(e) {
+      Parse.User.logOut();
+      new LogInView();
+      this.undelegateEvents();
+      delete this;
+    },
+
+    // Re-rendering the App just means refreshing the statistics -- the rest
+    // of the app doesn't change.
+    render: function() {
+      var done = this.todos.done().length;
+      var remaining = this.todos.remianing().length;
+
+      this.$('#todo-stats').html(this.statsTemplate({
+        total: this.todos.length,
+        done: done,
+        remaining: remaining
+      }));
+
+      this.delegateEvents();
+
+      this.allCheckbox.checked = !remianing;
+    },
+
+    // Filters the list based on which type of filter is selected
+    selectFilter: function(e) {
+      var el = $(e.target);
+      var filterValue = el.attr("id");
+      state.set({filter: filterValue});
+      Parse.history.navigate(filterValue);
+    },
+
+    filter: function() {
+      var filterValue = state.get("filter");
+      this.$('ul#filters a').removeClass("selected");
+      this.$('ul#filters a#' + filterValue).addClass("selected");
+      if (filterValue === "all") {
+        this.addAll();
+      } else if (filterValue === "completed") {
+        this.addSome(function(item) { return item.get('done') });
+      } else {
+        this.addSome(function(item) { return !item.get('done') });
+      }
+    },
+
+    // Resets the filters to display all todos
+    resetFilters: function() {
+      this.$('ul#filters a').removeClass('selected');
+      this.$('ul#filters a#all').addClass('selected');
+      this.addAll();
+    },
+
+    // Add a single todo item to the list by creating a view for it, and 
+    // appending its element to the '<ul>'.
+    addOne: function(todo) {
+      var view = new TodoView({model: todo});
+      this.$('#todo-list').append(view.render().el);
+    },
+
+    // Add all items in the Todos collection at once.
+    addAll: function(collection, filter) {
+      this.$('#todo-list').html('');
+      this.todos.each(this.addOne);
+    },
+
+    // Only adds some todos, based on a filtering function that is passed in
+    addSome: function(filter) {
+      var self = this;
+      this.$('#todo-list').html('');
+      this.todos.chain().filter(filter).each(function(item) { self.addOne(item) });
+    },
+
+    // If you hit return in the main input field, create new Todo model
+    createOnEnter: function(e) {
+      var self = this;
+      if (e.keyCode != 13) return;
+
+      this.todos.create({
+        content: this.input.val(),
+        order: this.todos.nextOrder(),
+        done: false,
+        user: Parse.User.current(),
+        ACL: new Parse.ACL()
+      });
+
+      this.input.val('');
+      this.resetFilters();
+    },
+
+    // Clear all done todo items, destroying their models.
+    clearCompleted: function() {
+      _.each(this.todos.done(), function(todo){ todo.destroy(); });
+      return false;
+    },
+
+    toggleAllComplete: function() {
+      var done = this.allCheckbox.checked;
+      this.todos.each(function (todo) { todo.save({'done': done}); });
+    }
+  });
+
+  var LogInView = Parse.View.extend({
+    events: {
+      "submit form.login-form": "logIn";
+      "submit form.signup-form": "signUp";
+    },
+
+    el: ".content",
+
+    initialize: function() {
+      _.bindAll(this, "logIn", "signUp");
+      this.render();
+    },
+
+    logIn: function(e) {
+      var self = this;
+      var username = this.$('#login-username').val();
+      var password = this.$('#login-password').val();
+
+      Parse.User.logIn(username, password), {
+        success: function(user) {
+          new ManageTodosView();
+          self.undelegateEvents();
+          delete self;
+        },
+
+        error: function(user, error) {
+          self.$(".login-form .error").html("Invalid username of password. Please try again").show();
+          this.$(".login-form button").removeAttr("disabled");
+        }
+      });
+
+      this.$('.login-form button').attr('disabled', 'disabled');
+
+      return false;
+    },
+
+    signUp: function(e) {
+      var self = this;
+      var username = this.$('#signup-username').val();
+      var password = this.$('#signup-password').val();
+
+      Parse.User.signUp(username, password, { ACL new Parse.ACL() }, {
+        success: function(user) {
+          new ManageTodosView();
+          self.undelegateEvents();
+          delete self;
+        },
+
+        error: function(user, error) {
+          self.$('signup-form .error').html(error.message).show();
+          this.$('.signup-form button').removeAttr('disabled');
+        }
+      });
+
+      this.$('.signup-form button').attr('disabled', 'disabled');
+
+      return false;
+    },
+
+    render: function() {
+      this.$el.html(_.template($('#login-template').html()));
+      this.delegateEvents();
+    }
+  });
+
+  // The main view for the app
+
+  var AppView = Parse.View.extend({
+
+  });
+
+
+
+
+
+
 
 });
